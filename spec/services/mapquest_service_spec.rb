0234f9debf1690f20d222ab9e936f7ebd40f89c7
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-16 15:34:10 -0700

    Update spec with correct expectations

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
-describe MapQuestService do
+describe MapquestService do
   context "instance methods" do
     context "#coordinates_by_city" do
       it "returns a longitude and latitude by a city name" do
         VCR.use_cassette("coordinates_by_city") do
-          @search = MapQuestService.coordinates_by_city("denver,co")
+          @search = MapquestService.coordinates_by_city("denver,co")
         end
 
         expect(@search).to be_a Hash
         expect(@search).to have_key :results
         expect(@search[:results]).to be_an Array
-        expect(@search[:results]).to have_key :locations
-        expect(@search[:results][:locations]).to be_an Array
-        expect(@search[:results][:locations]).to have_key :latLng
-        expect(@search[:results][:locations][:latLng]).to be_a Hash
-        expect(@search[:results][:locations][:latLng]).to have_key :lat
-        expect(@search[:results][:locations][:latLng][:lat]).to be_a Float
-        expect(@search[:results][:locations][:latLng]).to have_key :lng
-        expect(@search[:results][:locations][:latLng][:lng]).to be_a Float
+        expect(@search[:results][0]).to have_key :locations
+        expect(@search[:results][0][:locations]).to be_an Array
+        expect(@search[:results][0][:locations][0]).to have_key :latLng
+        expect(@search[:results][0][:locations][0][:latLng]).to be_a Hash
+        expect(@search[:results][0][:locations][0][:latLng]).to have_key :lat
+        expect(@search[:results][0][:locations][0][:latLng][:lat]).to be_a Float
+        expect(@search[:results][0][:locations][0][:latLng]).to have_key :lng
+        expect(@search[:results][0][:locations][0][:latLng][:lng]).to be_a Float
       end
     end
   end
