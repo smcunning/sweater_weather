98c4f754440e88f48bdafdf32c5f88b5055858d9
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-16 16:32:21 -0700

    Create open weather service with forecast method

---
+++ app/services/open_weather_service.rb
@@ -0,0 +1,19 @@
+class OpenWeatherService
+  def self.forecast(lat, lon)
+    results = conn.get('/data/2.5/onecall?') do |req|
+      req.params[:appid] = ENV['OPEN_WEATHER_API_KEY']
+      req.params[:lat] = lat
+      req.params[:lon] = lon
+      req.params[:exclude] = 'minutely'
+    end
+    parse(results)
+  end
+
+  def self.conn
+    Faraday.new(url: 'http://api.openweathermap.org')
+  end
+
+  def self.parse(results)
+    JSON.parse(results.body, symbolize_names: true)
+  end
+end
