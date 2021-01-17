a61c87ccfc5b963c32803a15ac80d2784b639ac9
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-17 15:54:53 -0700

    Create background poro

---
+++ app/poros/background.rb
@@ -0,0 +1,10 @@
+class Background
+  attr_reader  :id,
+               :image,
+               :credit
+  def initialize(attributes)
+    @id = 'null'
+    @image = Image.new(attributes[:results][0][:urls])
+    @credit = Credit.new(attributes[:results][0][:user])
+  end
+end
