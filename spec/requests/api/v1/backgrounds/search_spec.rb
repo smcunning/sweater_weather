12465a5cbdf727a01a4cab728a2649ef89df969a
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-17 15:06:17 -0700

    Add spec for backgrounds endpoint

---
+++ spec/requests/api/v1/backgrounds/search_spec.rb
@@ -0,0 +1,34 @@
+
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Backgrounds by Location Endpoint' do
+  it 'can return the an image for the location' do
+    VCR.use_cassette('denver-photos') do
+      get '/api/v1/backgrounds?location=denver,co'
+
+      expect(response).to be_successful
+
+      image_data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(image_data).to be_a Hash
+      expect(image_data).to have_key :id
+      expect(image_data[:id]).to eq('null')
+      expect(image_data).to have_key :type
+      expect(image_data[:type]).to eq('background')
+      expect(image_data).to have_key :attributes
+      expect(image_data[:attributes]).to have_key :image
+      expect(image_data[:attributes][:image]).to have_key :image_url
+      expect(image_data[:attributes][:image][:image_url]).to be_a String
+      expect(image_data[:attributes]).to have_key :credit
+      expect(image_data[:attributes][:credit]).to be_a Hash
+      expect(image_data[:attributes][:credit]).to have_key :author
+      expect(image_data[:attributes][:credit][:author]).to be_a String
+      expect(image_data[:attributes][:credit]).to have_key :source
+      expect(image_data[:attributes][:credit][:source]).to be_an Array
+      expect(image_data[:attributes][:credit]).to have_key :author_website
+      expect(image_data[:attributes][:credit][:author_website]).to be_a String
+    end
+  end
+end
