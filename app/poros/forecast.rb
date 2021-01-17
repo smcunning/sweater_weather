e2561b20792c030ce8cb8ddc161a56043c4661b9
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-17 00:25:26 -0700

    Create Forecast poro

---
+++ app/poros/forecast.rb
@@ -0,0 +1,25 @@
+class Forecast
+  attr_reader  :id,
+               :current,
+               :hourly,
+               :daily
+
+  def initialize(attributes)
+    @id = 'null'
+    @current = CurrentWeather.new(attributes[:current])
+    @hourly = hourly_weather(attributes[:hourly])
+    @daily = daily_weather(attributes[:daily])
+  end
+
+  def hourly_weather(data)
+    data.map do |hour|
+      HourlyWeather.new(hour)
+    end
+  end
+
+  def daily_weather(data)
+    data.map do |day|
+      DailyWeather.new(day)
+    end
+  end
+end
