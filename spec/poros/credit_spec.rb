f6054272010b9c275b24ecffbacf8407b585493c
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-17 16:15:59 -0700

    Create spec for credit poro

---
+++ spec/poros/credit_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Credit do
+  it 'exists with attributes' do
+  data = {
+    :id=>"8DPIZXj6JrA",
+      :updated_at=>"2021-01-16T19:50:02-05:00",
+      :username=>"sebbykurps",
+      :name=>"Sebastian Kurpiel",
+      :first_name=>"Sebastian",
+      :last_name=>"Kurpiel",
+      :twitter_username=>"Sebbykurps",
+      :portfolio_url=>nil,
+      :bio=>
+       "Just a software engineer with a passion for photography.\r\nFollow me on Instagram at @SebbyKurps, to see more of my adventures All photos are geotagged here, www.sebastiankurpiel.com",
+      :location=>nil,
+      :links=>
+       {
+         :self=>"https://api.unsplash.com/users/sebbykurps",
+         :html=>"https://unsplash.com/@sebbykurps",
+         :photos=>"https://api.unsplash.com/users/sebbykurps/photos",
+         :likes=>"https://api.unsplash.com/users/sebbykurps/likes",
+         :portfolio=>"https://api.unsplash.com/users/sebbykurps/portfolio",
+         :following=>"https://api.unsplash.com/users/sebbykurps/following",
+         :followers=>"https://api.unsplash.com/users/sebbykurps/followers"
+       }
+  }
+
+    credit = Credit.new(data)
+
+    expect(credit.author).to eq(data[:name])
+    expect(credit.source).to be_an Array
+    expect(credit.source[0]).to eq('unsplash.com')
+    expect(credit.source[1]).to eq(data[:name])
+    expect(credit.author_website).to eq(data[:links][:html])
+  end
+end
