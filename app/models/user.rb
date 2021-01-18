519ba882cf9f5bd539df302f3eef7391796c1472
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-17 19:47:55 -0700

    Create generate key and set api key methods to add as a before create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,23 @@
 require 'securerandom'
 
 class User < ApplicationRecord
-  has_secure_password
+  before_create :set_api_key
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
+
+  has_secure_password
+
+  private
+
+  def set_api_key
+    self.api_key = generate_key
+  end
+
+  def generate_key
+    loop do
+      key = SecureRandom.uuid
+      break key unless User.where(api_key: key).exists?
+    end
+  end
 end
