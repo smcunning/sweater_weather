88a66959abd0bb612f11b6ee0665906fa06f106d
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-18 11:46:43 -0700

    Update to use controller action for error handling

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,19 @@ module Api
   module V1
     class UsersController < ApplicationController
       def create
-        render json: UserFacade.create_user(request.body.string), status: :created
+        user_data = JSON.parse(request.body.string, symbolize_names: true)
+        user = User.create({
+          email: user_data[:email],
+          password: user_data[:password],
+          password_confirmation: user_data[:password_confirmation],
+          })
+
+        if user.save
+          render json: UserSerializer.new(user), status: :created
+        else
+          render json: { message: 'unsuccessful', error: user.errors.full_messages.to_sentence.to_s },
+                       status: :unprocessable_entity
+        end
       end
     end
   end
