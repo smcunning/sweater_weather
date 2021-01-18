da87e719938f593dc5615ece72767eb433891085
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-17 19:42:19 -0700

    Create users controller and show action

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def create
+        render json: UserFacade.create_user(request.body), status: :created
+      end
+    end
+  end
+end
