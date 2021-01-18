c2d77e07e2136f191c329061a10841f761894486
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-17 20:41:22 -0700

    Update spec with user create sad paths

--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -34,4 +34,61 @@ describe 'User Registration Endpoint' do
     expect(json[:data][:attributes]).to_not include(:password)
     expect(json[:data][:attributes]).to_not include(:password_digest)
   end
+
+  it 'cannot register a new user if passwords dont match' do
+    user_params = {
+                    email: "example@example.com",
+                    password: "password",
+                    password_confirmation: "password1"
+    }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/users', headers: headers, params: user_params, as: :json
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:status]).to eq('error')
+    expect(json[:code]).to eq(400)
+    expect(json[:message]).to eq("password and password confirmation  must match")
+  end
+
+  it 'cannot register a new user if field left blank' do
+    user_params = {
+                    email: "",
+                    password: "password",
+                    password_confirmation: "password1"
+    }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/users', headers: headers, params: user_params, as: :json
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:status]).to eq('error')
+    expect(json[:code]).to eq(400)
+    expect(json[:message]).to eq("email, password, and password confirmation must be present.")
+  end
+
+  it 'cannot register a new user if email already exists' do
+    user_params = {
+                    email: "",
+                    password: "password",
+                    password_confirmation: "password1"
+    }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/users', headers: headers, params: user_params, as: :json
+
+    #second post to attempt to create duplicate user
+    post '/api/v1/users', headers: headers, params: user_params, as: :json
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:status]).to eq('error')
+    expect(json[:code]).to eq(400)
+    expect(json[:message]).to eq("an account already exists for that email.")
+  end
 end
