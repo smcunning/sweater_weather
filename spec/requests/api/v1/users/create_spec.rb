5e662b3208a6416384b38fbf3bb99035ccaf4702
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-17 19:42:02 -0700

    Update request spec

--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -13,8 +13,9 @@ describe 'User Registration Endpoint' do
 
     headers = {"CONTENT_TYPE" => "application/json"}
 
-    post '/api/v1/users', headers: headers, params: JSON.generate(user_params)
+    post '/api/v1/users', headers: headers, params: user_params, as: :json
     created_user = User.last
+
     expect(response).to be_successful
     expect(response.status).to eq(201)
     expect(response.content_type).to include("application/json")
@@ -24,13 +25,13 @@ describe 'User Registration Endpoint' do
     expect(json).to have_key :data
     expect(json[:data]).to have_key :id
     expect(json[:data][:id]).to_not eq(nil)
-    expect(json)[:data][:id].to be_a String
+    expect(json[:data][:id]).to be_a String
     expect(json[:data]).to have_key :type
-    expect(json[:data][:type]).to eq('users')
+    expect(json[:data][:type]).to eq('user')
     expect(json[:data]).to have_key :attributes
-    expec(json[:data][:attributes]).to have_key :email
-    expec(json[:data][:attributes]).to have_key :api_key
-    expec(json[:data][:attributes]).to_not include(:password)
-    expec(json[:data][:attributes]).to_not include(:password_digest)
+    expect(json[:data][:attributes]).to have_key :email
+    expect(json[:data][:attributes]).to have_key :api_key
+    expect(json[:data][:attributes]).to_not include(:password)
+    expect(json[:data][:attributes]).to_not include(:password_digest)
   end
 end
