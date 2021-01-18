49ebea2bf61976ffc8282f360b63e78e088ba2a0
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-18 13:08:18 -0700

    Create sessions controller with create action

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class SessionsController < ApplicationController
+      def create
+        login_creds = JSON.parse(request.body.string, symbolize_names: true)
+        user = User.find_by(email: login_creds[:email])
+        if user && user.authenticate(login_creds[:password])
+          render json: UserSerializer.new(user), status: :ok
+        else
+          render json: { message: 'unsuccessful', error: 'Credentials are bad.' },
+                 status: :unauthorized
+        end
+      end
+    end
+  end
+end
