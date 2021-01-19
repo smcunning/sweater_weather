f0562a264c6ed98f3625789793b06463a0a0302b
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-19 09:30:35 -0700

    Create yelp service

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class YelpService
-  def self.businesses(location)
+  def self.businesses(location, open_at, category)
     results = conn.get('/v3/businesses/search?') do |req|
       req.headers["Authorization"] = "Bearer #{ENV.fetch("YELP_API_KEY")}"
       req.params[:location] = location
+      req.params[:open_at] = open_at
+      req.params[:category] = category
     end
     parse(results)
   end
