ce3a5256c4014583431c9598ef31eeefd1b0fa47
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-19 10:48:49 -0700

    Create Munchie poro spec

---
+++ spec/poros/munchie_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Munchie do
+  it 'exists with attributes' do
+    VCR.use_cassette('munchies-data') do
+      munchie = MunchiesFacade.munchies_details('denver,co', 'pueblo,co', 'chinese')
+
+      expect(munchie.id).to eq('null')
+      expect(munchie.forecast).to be_a Hash
+      expect(munchie.forecast).to have_key :summary
+      expect(munchie.forecast[:summary]).to be_a String
+      expect(munchie.forecast).to have_key :temperature
+      expect(munchie.forecast[:temperature]).to be_a Float
+      expect(munchie.travel_time).to be_a String
+      expect(munchie.destination_city).to be_a String
+      expect(munchie.restaurant).to be_a Hash
+      expect(munchie.restaurant).to have_key :name
+      expect(munchie.restaurant[:name]).to be_a String
+      expect(munchie.restaurant).to have_key :address
+      expect(munchie.restaurant[:address]).to be_a String
+    end
+  end
+end
