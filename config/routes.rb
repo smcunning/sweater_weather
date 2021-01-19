841252a42a15b6fae73a3699b4cd7c06dbf81766
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-19 09:18:14 -0700

    Create route to munchies controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#create'
       post '/road_trip', to: 'road_trip#create'
+      get '/munchies', to: 'munchies#show'
     end
   end
 end
