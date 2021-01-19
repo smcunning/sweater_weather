06e93a1494dd4441e4e46fd2873ced4b55b5acac
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2021-01-19 10:50:01 -0700

    Create munchie poro

---
+++ app/poros/munchie.rb
@@ -0,0 +1,16 @@
+class Munchie
+  include Convertable
+  attr_reader :id,
+              :destination_city,
+              :travel_time,
+              :forecast,
+              :restaurant
+
+  def initialize(attributes)
+    @id = 'null'
+    @destination_city = attributes[:destination_city]
+    @travel_time = convert_time_with_words(attributes[:travel_time][:formatted])
+    @forecast = attributes[:forecast]
+    @restaurant = attributes[:restaurant]
+  end
+end
